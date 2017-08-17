946fd5c0ab8240d59271319a0698290b579f6db1
krishnakanth27 <31101176+krishnakanth27@users.noreply.github.com>
2017-08-17 22:51:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-
 });
 
 app.get('/first-article', function (req, res) {
