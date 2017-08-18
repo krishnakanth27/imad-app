446a3daacafce6a038cb529040f892c14a239f2a
krishnakanth27 <31101176+krishnakanth27@users.noreply.github.com>
2017-08-18 21:46:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML='krishnakanth'
-var img=document.getelementById('madi');
+var img=document.getElementById('madi');
 img.onclick = function() {
     img.style.marginleft='100px';
 };
