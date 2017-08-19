41882eb98550c4fd1c8c1bc0474abaa14bed921e
krishnakanth27 <31101176+krishnakanth27@users.noreply.github.com>
2017-08-19 07:13:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,16 @@
-console.log('Loaded!');
-var element = document.getElementById('main-text');
-element.innerHTML='krishnakanth'
-var img=document.getElementById('madi');
-var marginLeft = 0;
-function moveRight() {
-    marginLeft=marginLeft + 1;
-    img.style.marginLeft=marginLeft+'px';
-}
-img.onclick = function() {
-   var interval=setInterval(moveRight,50);
+var button=document.getElementById('counter');
+var counter=0;
+button.onclick=function(){
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function() {
+        if(request.readystate=== XMLHttpRequest.Done) {
+            if(request.status===200){
+            var counter =request.responseText;
+            var span =document.getElementById('count');
+            span.innerHTML=counter.toString();
+        }
+    }
+};
+request.open('GET','http://u14311a05t4.imad.hasura-app.io/counter',true);
+request.send(null);
 };
