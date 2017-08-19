eec2066a6f9999b86fd32960e162d481839c78b3
krishnakanth27 <31101176+krishnakanth27@users.noreply.github.com>
2017-08-19 07:16:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -64,14 +64,16 @@ var htmltemplate = `
 return htmltemplate;
 }
 
+
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
 var counter=0;
 app.get('/counter', function (req,res){
     counter=counter + 1;
     res.send(counter , toString());
-});
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/:articleName' , function (req, res) {
